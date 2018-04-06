66844e962f07f5958c5ecc622bda8b426cc285a0
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-04-06 09:51:11 -0700

    stock detail view test added

--- stock_portfolio/stock_portfolio/tests/conftest.py
+++ stock_portfolio/stock_portfolio/tests/conftest.py
@@ -5,3 +5,13 @@ from pyramid import testing
 @pytest.fixture
 def dummy_request():
     return testing.DummyRequest()
+
+
+@pytest.fixture
+def dummy_auth_request():
+    from pyramid.request import Request
+    request = testing.DummyRequest()
+    request.method = 'POST'
+    request.POST = {'username': 'dummy', 'password': '1234'}
+
+    return request
